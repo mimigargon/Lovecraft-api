084b3649e986fb15c3f5d7fa24e2bb91c1c4929e
mimigargon <mireiag77@gmail.com>
2022-06-02 13:02:40 +0200

    API started

---
+++ src/utils/database/db.js
@@ -0,0 +1,20 @@
+const dotenv = require('dotenv');
+dotenv.config();
+const mongoose = require('mongoose');
+const urlDb = process.env.MONGO_DB;
+
+const connectDb = async () => {
+    try {
+        const db = await mongoose.connect(urlDb, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true, 
+        });
+
+        const { name, host } = db.connection;
+        console.log(`Connected with db name: ${name} in host: ${host}`);
+    } catch (error) {
+        console.log('Error to connect with db', error);
+    }
+};
+
+module.exports = { connectDb }; 
